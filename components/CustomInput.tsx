a75c5960f2369f4f29c834f60d7adab46804e2d5
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-09-17 20:13:37 +0530

    Initial Commit

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -6,9 +6,12 @@ import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
 
+const formSchema = authFormSchema('sign-up')
+
+
 interface CustomInput {
-    control: Control<z.infer<typeof authFormSchema>>,
-    name: FieldPath<z.infer<typeof authFormSchema>>,
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder: string,
 }
@@ -29,7 +32,7 @@ const CustomInput = ({control, name, label, placeholder} : CustomInput) => {
         <Input
             placeholder={placeholder}
             className='input-class'
-            
+            type={name === 'password' ? 'password' : 'text'}
             {...field} />
         </FormControl>
         <FormMessage className='form-message mt-2'/>
