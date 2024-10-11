522d656229773c477c250b8aa2500ec19c37aab2
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-10-11 21:09:29 +0530

    Commit

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,20 +1,31 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import {PlaidLinkOptions} from 'react-plaid-link'
+import { Button } from './ui/button';
+import { StyledString } from 'next/dist/build/swc';
+import { useRouter } from 'next/navigation';
 
 const PlaidLink = ({user, variant} : PlaidLinkProps) => {
+
+    const router = useRouter();
 
     const [token,setToken] = useState('');
 
     useEffect(() => {
         const getLinkToken = async () => {
-            const data = await createLinkToken(user);
+            //const data = await createLinkToken(user);
+            //setToken(data?.linkToken);
         }
 
         getLinkToken();
     }, [])
 
-    const onSuccess = useCallback( async () => {
+    const onSuccess = useCallback( async (public_token:StyledString) => {
+        // await exchangePublicToken({
+        //   publicToken: public_token,
+        //   user,
+        // })
 
+        router.push('/');
     }, [user])
 
     const config : PlaidLinkOptions = {
@@ -23,7 +34,24 @@ const PlaidLink = ({user, variant} : PlaidLinkProps) => {
     }
 
   return (
-    <div>PlaidLink</div>
+    <>
+    {variant === 'primary' ? (
+      <Button
+        className='plaidlink-primary'
+        >
+          Connect Bank
+        </Button>
+    ): variant === 'ghost' ? (
+      <Button>
+        Connect Bank
+      </Button>
+    ): (
+      <Button>
+        Connect Bank
+      </Button>
+    )
+  }
+    </>
   )
 }
 
