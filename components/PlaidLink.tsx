9c9d1bdad54261c6867e67155a0890494dde006c
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-10-12 17:40:59 +0530

    commit

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,58 +1,64 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import {PlaidLinkOptions} from 'react-plaid-link'
-import { Button } from './ui/button';
-import { StyledString } from 'next/dist/build/swc';
-import { useRouter } from 'next/navigation';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link";
+import { Button } from "./ui/button";
+import { StyledString } from "next/dist/build/swc";
+import { useRouter } from "next/navigation";
+import { createLinkToken } from "@/lib/actions/user.actions";
 
-const PlaidLink = ({user, variant} : PlaidLinkProps) => {
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const [token, setToken] = useState("");
 
-    const [token,setToken] = useState('');
+  useEffect(() => {
+    const getLinkToken = async () => {
+      const data = await createLinkToken(user);
+      setToken(data?.linkToken);
+    };
 
-    useEffect(() => {
-        const getLinkToken = async () => {
-            //const data = await createLinkToken(user);
-            //setToken(data?.linkToken);
-        }
+    getLinkToken();
+  }, [user]);
 
-        getLinkToken();
-    }, [])
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
+      // await exchangePublicToken({
+      //   publicToken: public_token,
+      //   user,
+      // })
 
-    const onSuccess = useCallback( async (public_token:StyledString) => {
-        // await exchangePublicToken({
-        //   publicToken: public_token,
-        //   user,
-        // })
+      router.push("/");
+    },
+    [user]
+  );
 
-        router.push('/');
-    }, [user])
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
 
-    const config : PlaidLinkOptions = {
-        token,
-        onSuccess
-    }
+  const { open, ready } = usePlaidLink(config);
 
   return (
     <>
-    {variant === 'primary' ? (
-      <Button
-        className='plaidlink-primary'
+      {variant === "primary" ? (
+        <Button
+          onClick={() => open()}
+          disabled={!ready}
+          className="plaidlink-primary"
         >
           Connect Bank
         </Button>
-    ): variant === 'ghost' ? (
-      <Button>
-        Connect Bank
-      </Button>
-    ): (
-      <Button>
-        Connect Bank
-      </Button>
-    )
-  }
+      ) : variant === "ghost" ? (
+        <Button>Connect Bank</Button>
+      ) : (
+        <Button>Connect Bank</Button>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default PlaidLink
+export default PlaidLink;
